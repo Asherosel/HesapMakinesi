fffc30152e42684c200538db8e6ba8055cdfdb1f
Asherosel <Ibrahim.Karatas.07@hotmail.com>
2024-03-23 18:17:09 +0300

    Add project files.

---
+++ HesapMakinesi/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace HesapMakinesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            button14 = new Button();
+            button15 = new Button();
+            numberLabel = new Label();
+            button16 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Cursor = Cursors.Hand;
+            button1.Location = new Point(12, 73);
+            button1.Name = "button1";
+            button1.Size = new Size(57, 51);
+            button1.TabIndex = 0;
+            button1.Text = "1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Cursor = Cursors.Hand;
+            button2.Location = new Point(75, 73);
+            button2.Name = "button2";
+            button2.Size = new Size(57, 51);
+            button2.TabIndex = 1;
+            button2.Text = "2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Cursor = Cursors.Hand;
+            button3.Location = new Point(138, 73);
+            button3.Name = "button3";
+            button3.Size = new Size(57, 51);
+            button3.TabIndex = 2;
+            button3.Text = "3";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Cursor = Cursors.Hand;
+            button4.Location = new Point(12, 130);
+            button4.Name = "button4";
+            button4.Size = new Size(57, 51);
+            button4.TabIndex = 3;
+            button4.Text = "4";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Cursor = Cursors.Hand;
+            button5.Location = new Point(75, 130);
+            button5.Name = "button5";
+            button5.Size = new Size(57, 51);
+            button5.TabIndex = 4;
+            button5.Text = "5";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Cursor = Cursors.Hand;
+            button6.Location = new Point(138, 130);
+            button6.Name = "button6";
+            button6.Size = new Size(57, 51);
+            button6.TabIndex = 5;
+            button6.Text = "6";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.Cursor = Cursors.Hand;
+            button7.Location = new Point(12, 187);
+            button7.Name = "button7";
+            button7.Size = new Size(57, 51);
+            button7.TabIndex = 6;
+            button7.Text = "7";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // button8
+            // 
+            button8.Cursor = Cursors.Hand;
+            button8.Location = new Point(75, 187);
+            button8.Name = "button8";
+            button8.Size = new Size(57, 51);
+            button8.TabIndex = 7;
+            button8.Text = "8";
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += button8_Click;
+            // 
+            // button9
+            // 
+            button9.Cursor = Cursors.Hand;
+            button9.Location = new Point(138, 187);
+            button9.Name = "button9";
+            button9.Size = new Size(57, 51);
+            button9.TabIndex = 8;
+            button9.Text = "9";
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += button9_Click;
+            // 
+            // button10
+            // 
+            button10.Cursor = Cursors.Hand;
+            button10.Location = new Point(75, 244);
+            button10.Name = "button10";
+            button10.Size = new Size(57, 51);
+            button10.TabIndex = 9;
+            button10.Text = "0";
+            button10.UseVisualStyleBackColor = true;
+            button10.Click += button10_Click;
+            // 
+            // button11
+            // 
+            button11.Cursor = Cursors.Hand;
+            button11.Location = new Point(201, 244);
+            button11.Name = "button11";
+            button11.Size = new Size(57, 51);
+            button11.TabIndex = 10;
+            button11.Text = "=";
+            button11.UseVisualStyleBackColor = true;
+            button11.Click += button11_Click;
+            // 
+            // button12
+            // 
+            button12.Cursor = Cursors.Hand;
+            button12.Location = new Point(201, 187);
+            button12.Name = "button12";
+            button12.Size = new Size(57, 51);
+            button12.TabIndex = 11;
+            button12.Text = "+";
+            button12.UseVisualStyleBackColor = true;
+            button12.Click += button12_Click;
+            // 
+            // button13
+            // 
+            button13.Cursor = Cursors.Hand;
+            button13.Location = new Point(201, 130);
+            button13.Name = "button13";
+            button13.Size = new Size(57, 51);
+            button13.TabIndex = 12;
+            button13.Text = "-";
+            button13.UseVisualStyleBackColor = true;
+            button13.Click += button13_Click;
+            // 
+            // button14
+            // 
+            button14.Cursor = Cursors.Hand;
+            button14.Location = new Point(201, 73);
+            button14.Name = "button14";
+            button14.Size = new Size(57, 51);
+            button14.TabIndex = 13;
+            button14.Text = "x";
+            button14.UseVisualStyleBackColor = true;
+            button14.Click += button14_Click;
+            // 
+            // button15
+            // 
+            button15.Cursor = Cursors.Hand;
+            button15.Location = new Point(138, 244);
+            button15.Name = "button15";
+            button15.Size = new Size(57, 51);
+            button15.TabIndex = 14;
+            button15.Text = "÷";
+            button15.UseVisualStyleBackColor = true;
+            button15.Click += button15_Click;
+            // 
+            // numberLabel
+            // 
+            numberLabel.AutoSize = true;
+            numberLabel.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            numberLabel.Location = new Point(12, 20);
+            numberLabel.Name = "numberLabel";
+            numberLabel.RightToLeft = RightToLeft.Yes;
+            numberLabel.Size = new Size(0, 37);
+            numberLabel.TabIndex = 15;
+            // 
+            // button16
+            // 
+            button16.Location = new Point(12, 244);
+            button16.Name = "button16";
+            button16.Size = new Size(57, 51);
+            button16.TabIndex = 16;
+            button16.Text = "C";
+            button16.UseVisualStyleBackColor = true;
+            button16.Click += button16_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(272, 308);
+            Controls.Add(button16);
+            Controls.Add(numberLabel);
+            Controls.Add(button15);
+            Controls.Add(button14);
+            Controls.Add(button13);
+            Controls.Add(button12);
+            Controls.Add(button11);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Hesap Makinesi";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Button button13;
+        private Button button14;
+        private Button button15;
+        private Label numberLabel;
+        private Button button16;
+    }
+}
---
+++ HesapMakinesi/Form1.cs
@@ -0,0 +1,417 @@
+namespace HesapMakinesi
+{
+    public partial class Form1 : Form
+    {
+        double number1 = 0;
+        double number2 = 0;
+        string islem;
+        bool islemyapildi = false;
+        bool birincisayidolumu = false;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (islemyapildi == true)
+            {
+                numberLabel.Text = "";
+                islemyapildi = false;
+                if (birincisayidolumu == false)
+                {
+                    numberLabel.Text += "1";
+                    number1 = Double.Parse(numberLabel.Text);
+                }
+                else
+                {
+                    numberLabel.Text += "1";
+                    number2 = Double.Parse(numberLabel.Text);
+                }
+            }
+            else
+            {
+                if (birincisayidolumu == false)
+                {
+                    numberLabel.Text += "1";
+                    number1 = Double.Parse(numberLabel.Text);
+                }
+                else
+                {
+                    numberLabel.Text += "1";
+                    number2 = Double.Parse(numberLabel.Text);
+                }
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (islemyapildi == true)
+            {
+                numberLabel.Text = "";
+                islemyapildi = false;
+                if (birincisayidolumu == false)
+                {
+                    numberLabel.Text += "2";
+                    number1 = Double.Parse(numberLabel.Text);
+                }
+                else
+                {
+                    numberLabel.Text += "2";
+                    number2 = Double.Parse(numberLabel.Text);
+                }
+            }
+            else
+            {
+                if (birincisayidolumu == false)
+                {
+                    numberLabel.Text += "2";
+                    number1 = Double.Parse(numberLabel.Text);
+                }
+                else
+                {
+                    numberLabel.Text += "2";
+                    number2 = Double.Parse(numberLabel.Text);
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (islemyapildi == true)
+            {
+                numberLabel.Text = "";
+                islemyapildi = false;
+                if (birincisayidolumu == false)
+                {
+                    numberLabel.Text += "3";
+                    number1 = Double.Parse(numberLabel.Text);
+                }
+                else
+                {
+                    numberLabel.Text += "3";
+                    number2 = Double.Parse(numberLabel.Text);
+                }
+            }
+            else
+            {
+                if (birincisayidolumu == false)
+                {
+                    numberLabel.Text += "3";
+                    number1 = Double.Parse(numberLabel.Text);
+                }
+                else
+                {
+                    numberLabel.Text += "3";
+                    number2 = Double.Parse(numberLabel.Text);
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (islemyapildi == true)
+            {
+                numberLabel.Text = "";
+                islemyapildi = false;
+                if (birincisayidolumu == false)
+                {
+                    numberLabel.Text += "4";
+                    number1 = Double.Parse(numberLabel.Text);
+                }
+                else
+                {
+                    numberLabel.Text += "4";
+                    number2 = Double.Parse(numberLabel.Text);
+                }
+            }
+            else
+            {
+                if (birincisayidolumu == false)
+                {
+                    numberLabel.Text += "4";
+                    number1 = Double.Parse(numberLabel.Text);
+                }
+                else
+                {
+                    numberLabel.Text += "4";
+                    number2 = Double.Parse(numberLabel.Text);
+                }
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (islemyapildi == true)
+            {
+                numberLabel.Text = "";
+                islemyapildi = false;
+                if (birincisayidolumu == false)
+                {
+                    numberLabel.Text += "5";
+                    number1 = Double.Parse(numberLabel.Text);
+                }
+                else
+                {
+                    numberLabel.Text += "5";
+                    number2 = Double.Parse(numberLabel.Text);
+                }
+            }
+            else
+            {
+                if (birincisayidolumu == false)
+                {
+                    numberLabel.Text += "5";
+                    number1 = Double.Parse(numberLabel.Text);
+                }
+                else
+                {
+                    numberLabel.Text += "5";
+                    number2 = Double.Parse(numberLabel.Text);
+                }
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (islemyapildi == true)
+            {
+                numberLabel.Text = "";
+                islemyapildi = false;
+                if (birincisayidolumu == false)
+                {
+                    numberLabel.Text += "6";
+                    number1 = Double.Parse(numberLabel.Text);
+                }
+                else
+                {
+                    numberLabel.Text += "6";
+                    number2 = Double.Parse(numberLabel.Text);
+                }
+            }
+            else
+            {
+                if (birincisayidolumu == false)
+                {
+                    numberLabel.Text += "6";
+                    number1 = Double.Parse(numberLabel.Text);
+                }
+                else
+                {
+                    numberLabel.Text += "6";
+                    number2 = Double.Parse(numberLabel.Text);
+                }
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (islemyapildi == true)
+            {
+                numberLabel.Text = "";
+                islemyapildi = false;
+                if (birincisayidolumu == false)
+                {
+                    numberLabel.Text += "7";
+                    number1 = Double.Parse(numberLabel.Text);
+                }
+                else
+                {
+                    numberLabel.Text += "7";
+                    number2 = Double.Parse(numberLabel.Text);
+                }
+            }
+            else
+            {
+                if (birincisayidolumu == false)
+                {
+                    numberLabel.Text += "7";
+                    number1 = Double.Parse(numberLabel.Text);
+                }
+                else
+                {
+                    numberLabel.Text += "7";
+                    number2 = Double.Parse(numberLabel.Text);
+                }
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (islemyapildi == true)
+            {
+                numberLabel.Text = "";
+                islemyapildi = false;
+                if (birincisayidolumu == false)
+                {
+                    numberLabel.Text += "8";
+                    number1 = Double.Parse(numberLabel.Text);
+                }
+                else
+                {
+                    numberLabel.Text += "8";
+                    number2 = Double.Parse(numberLabel.Text);
+                }
+            }
+            else
+            {
+                if (birincisayidolumu == false)
+                {
+                    numberLabel.Text += "8";
+                    number1 = Double.Parse(numberLabel.Text);
+                }
+                else
+                {
+                    numberLabel.Text += "8";
+                    number2 = Double.Parse(numberLabel.Text);
+                }
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (islemyapildi == true)
+            {
+                numberLabel.Text = "";
+                islemyapildi = false;
+                if (birincisayidolumu == false)
+                {
+                    numberLabel.Text += "9";
+                    number1 = Double.Parse(numberLabel.Text);
+                }
+                else
+                {
+                    numberLabel.Text += "9";
+                    number2 = Double.Parse(numberLabel.Text);
+                }
+            }
+            else
+            {
+                if (birincisayidolumu == false)
+                {
+                    numberLabel.Text += "9";
+                    number1 = Double.Parse(numberLabel.Text);
+                }
+                else
+                {
+                    numberLabel.Text += "9";
+                    number2 = Double.Parse(numberLabel.Text);
+                }
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if (islemyapildi == true)
+            {
+                numberLabel.Text = "";
+                islemyapildi = false;
+                if (birincisayidolumu == false)
+                {
+                    numberLabel.Text += "0";
+                    number1 = Double.Parse(numberLabel.Text);
+                }
+                else
+                {
+                    numberLabel.Text += "0";
+                    number2 = Double.Parse(numberLabel.Text);
+                }
+            }
+            else
+            {
+                if (birincisayidolumu == false)
+                {
+                    numberLabel.Text += "0";
+                    number1 = Double.Parse(numberLabel.Text);
+                }
+                else
+                {
+                    numberLabel.Text += "0";
+                    number2 = Double.Parse(numberLabel.Text);
+                }
+            }
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            if (islem == "+")
+            {
+                numberLabel.Text = (number1 + number2).ToString();
+                islemyapildi = true;
+                birincisayidolumu = false;
+                number1 = 0;
+                number2 = 0;
+                islem = null;
+            }
+
+            if (islem == "-")
+            {
+                numberLabel.Text = (number1 - number2).ToString();
+                islemyapildi = true;
+                birincisayidolumu = false;
+                number1 = 0;
+                number2 = 0;
+                islem = null;
+            }
+
+            if (islem == "/")
+            {
+                numberLabel.Text = (number1 / number2).ToString();
+                islemyapildi = true;
+                birincisayidolumu = false;
+                number1 = 0;
+                number2 = 0;
+                islem = null;
+            }
+
+            if (islem == "*")
+            {
+                numberLabel.Text = (number1 * number2).ToString();
+                islemyapildi = true;
+                birincisayidolumu = false;
+                number1 = 0;
+                number2 = 0;
+                islem = null;
+            }
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            numberLabel.Text = "";
+            islem = "+";
+            birincisayidolumu = true;
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            numberLabel.Text = "";
+            islem = "*";
+            birincisayidolumu = true;
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            numberLabel.Text = "";
+            islem = "-";
+            birincisayidolumu = true;
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            numberLabel.Text = "";
+            islem = "/";
+            birincisayidolumu = true;
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            numberLabel.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
